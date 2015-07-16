dc5762cd8877ca0ea589f37715d05655c1c398a9
zhaoyayu <zhaoyayu>
2015-07-16 11:33:21 +0800

    下拉刷新,上拉加载优化

---
+++ src/zyy/ticket/constant/Constant.java
@@ -0,0 +1,20 @@
+package zyy.ticket.constant;
+
+
+/**
+ * 常量类
+ * @author zhaoyayu
+ *
+ */
+public class Constant {
+
+    public static final int REFRESH_TYPE_PULL = 0;
+
+    public static final int REFRESH_TYPE_MORE = 1;
+
+    /** 列表加载每页数量 **/
+    public static int listLoadNum = 10;
+
+    public static final int[] PULL_COLORS = { android.R.color.holo_blue_bright, android.R.color.holo_green_light, android.R.color.holo_orange_light,
+            android.R.color.holo_red_light };
+}
